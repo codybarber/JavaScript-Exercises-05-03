5fee278a186f3a850e02215e947d41711d92ac4d
Cody Barber <cody.n.barber@gmail.com>
2016-05-09 16:09:22 -0400

    a couple of little edits

--- leetspeak.js
+++ leetspeak.js
@@ -1,19 +1,14 @@
 /*Enter text to convert to l33tsp34k*/
 var text = "Given a paragraph of text as a string, print the paragraph in leetspeak. To translate a string to leetspeak, you need to replace make the following character replacements (treat all input characters as uppercase)";
 
-var leet = text.replace(/a/g, "4");
-var leet2 = leet.replace(/A/g, "4");
-var leet3 = leet2.replace(/e/g, "3");
-var leet4 = leet3.replace(/E/g, "3");
-var leet5 = leet4.replace(/g/g, "6");
-var leet6 = leet5.replace(/G/g, "6");
-var leet7 = leet6.replace(/i/g, "1");
-var leet8 = leet7.replace(/I/g, "1");
-var leet9 = leet8.replace(/o/g, "0");
-var leet10 = leet9.replace(/O/g, "0");
-var leet11 = leet10.replace(/s/g, "5");
-var leet12 = leet11.replace(/S/g, "5");
-var leet13 = leet12.replace(/t/g, "7");
-var leet14 = leet13.replace(/T/g, "7");
+var textUpper = text.toUpperCase()
 
-console.log(leet14);
+var leet1 = textUpper.replace(/A/g, "4");
+var leet2 = leet1.replace(/E/g, "3");
+var leet3 = leet2.replace(/G/g, "6");
+var leet4 = leet3.replace(/I/g, "1");
+var leet5 = leet4.replace(/O/g, "0");
+var leet6 = leet5.replace(/S/g, "5");
+var leet7 = leet6.replace(/T/g, "7");
+
+console.log(leet7);
--- reverseastring.js
+++ reverseastring.js
@@ -7,3 +7,18 @@ function reverse(str) {
 }
 
 console.log(reverse(string));
+
+
+/*
+var string = "JavaScript";
+var length = string.length;
+var reversedString = "";
+var counter = length - 1;
+while (counter >= 0) {
+  var letter = string.charAt(counter);
+  reversedString = reversedString + letter;
+  counter--;
+}
+
+console.log(reversedString);
+*/
